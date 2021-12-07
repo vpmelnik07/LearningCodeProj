870bb8bfebe55a65965dbcba4468e37403ecae5a
vpmelnik07 <94337230+vpmelnik07@users.noreply.github.com>
2021-12-06 22:16:51 -0500

    pushing variables dir and learnings
    reviewed string variable modifications and simple numerical operations. pushing to git repo

---
+++ learing the basics/python/1.variables/3.numbers.py
@@ -0,0 +1,54 @@
+#simple numerical operations
+
+#5 addition
+print(2+3)
+
+#2 subtraction
+print(5-3)
+
+#8 multiplication
+print(2*4)
+
+#1.5
+print(3/2)
+
+#4.0 when you divide and the result is a whole number, it's written as a float
+print(8/2)
+
+#16 power
+print(2**4)
+
+#floating value expressions, if any one number is a float, the default output is a float
+
+#0.2
+print(0.1+0.1)
+
+#0.30000000000000004
+print(0.4-0.1)
+
+#0.04000000000000001
+print(0.2*0.2)
+
+#underscores in numbers for readability, all underscores regardless of grouping are ignored
+
+hug_number = 123_000_000_000
+
+#123000000000
+print(hug_number)
+
+#multiple variable assignments
+
+x, y, z = 0, 1, 2
+
+#0
+print(x)
+
+#1
+print(y)
+
+#2
+print(z)
+
+#constants - non changing variables, programmer practice suggests all caps for constants vars. Do not change these
+
+MAX_USERS = 100
